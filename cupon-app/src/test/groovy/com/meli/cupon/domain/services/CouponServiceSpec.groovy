25a79763f531cc0ccee33cb44073acb6b5bb8d92
anthonydicortes <141470136+anthonydicortes@users.noreply.github.com>
2023-08-05 00:19:20 -0500

    Merge pull request #2
    * fix naming

--- cupon-app/src/test/groovy/com/meli/cupon/domain/services/CouponServiceSpec.groovy
+++
@@ -1,9 +0,0 @@
-package com.meli.cupon.domain.services
-
-import spock.lang.Ignore
-import spock.lang.Specification
-
-@Ignore
-class CouponServiceSpec extends Specification {
-
-}
---
+++ cupon-app/src/test/groovy/com/meli/login/domain/model/entities/ItemSpec.groovy
@@ -0,0 +1,35 @@
+package com.meli.login.domain.model.entities
+
+import spock.lang.Specification
+
+class ItemSpec extends Specification {
+
+    def "Item instance return IllegalArgumentException"() {
+        when:
+        new Item(new IdItem("MLA1"), valor)
+
+        then:
+        def exception = thrown(IllegalArgumentException)
+        exception.message == mensajeEsperado
+
+        where:
+        valor                  || mensajeEsperado
+        BigDecimal.valueOf(0)  || "Invalid value."
+        BigDecimal.valueOf(-1) || "Invalid value."
+
+    }
+
+    def "Valid item instance"() {
+        given:
+        def itemValue = BigDecimal.valueOf(100)
+
+        when:
+        def item = new Item(new IdItem("MLA1"), itemValue)
+
+        then:
+        with(item) {
+            itemId() == new IdItem("MLA1")
+            value() == BigDecimal.valueOf(100)
+        }
+    }
+}
